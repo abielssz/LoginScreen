0f0e86ac2e2ec6be3f4c4a316cd3285f06629098
abielssz <abielssz16@hotmail.com>
2023-10-09 19:29:55 -0300

    1 commit do login

---
+++ script.js
@@ -0,0 +1,25 @@
+// const number1 = prompt ('informe o primeiro salário');
+
+// const number2 = prompt ('aumente 30% ');
+
+// const result = number1 /100 * 10;
+
+// let resultnumbers = alert(`o resultado é ${result}:)`);
+
+
+let num1 = 10;
+
+let num2 = 5;
+
+let num3 =7;
+
+let num4 = 2
+
+const result = num1 + num2 + num3 + num4 / 4;
+
+if (result >=5 ){
+    console.log ('você passou de ano')
+} 
+else {
+    console.log ('você está de recuperação');
+}
